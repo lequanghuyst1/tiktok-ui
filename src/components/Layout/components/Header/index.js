fefdec777c13b96968a89b94f482c77d96bdbcdf
lqhuy0712 <lequanghuyst1@gmail.com>
2023-08-07 20:29:21 +0700

    handle logic header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,10 @@
-import { useEffect, useState } from 'react';
+
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faCircleQuestion,
-    faCircleXmark,
-    faCloudArrowUp,
     faCoins,
     faEarthAsia,
-    faEllipsis,
     faEllipsisVertical,
     faGear,
     faKeyboard,
@@ -18,18 +15,17 @@ import {
     faSpinner,
 } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react';
-import HeadlessTippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
 
-import { Wrapper as WrapperProper } from '~/components/Layout/Proper';
 import styles from './header.module.scss';
 import images from '~/assets/images';
-import AccountItem from '~/components/Layout/AccountItem';
 import Button from '~/components/Button';
 import Menu from '~/components/Layout/Proper/Menu';
-import { faMessage, faUser } from '@fortawesome/free-regular-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { InboxIcon, MessageIcon, SearchIcon, UploadIcon } from '~/components/Icons';
 import Image from '~/components/Images';
+import Search from '../Search';
+
 const cx = classNames.bind(styles);
 
 const MENU_ITEMS = [
@@ -66,12 +62,6 @@ const MENU_ITEMS = [
 const currentUser = true;
 
 function Header() {
-    const [searchResult, setSearchResult] = useState([]);
-    useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([]);
-        }, 1000);
-    }, []);
 
     //handle Logic
     const handleMenuChange = (menuItem) => {
@@ -114,33 +104,8 @@ function Header() {
                 <div className={cx('logo')}>
                     <img src={images.logo} alt="Check"></img>
                 </div>
-                <HeadlessTippy
-                    interactive
-                    visible={searchResult.length > 0}
-                    render={(attrs) => (
-                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                            <WrapperProper>
-                                <h4 className={cx('search-title')}>Account</h4>
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                            </WrapperProper>
-                        </div>
-                    )}
-                    content="Tìm kiếm"
-                >
-                    <div className={cx('search')}>
-                        <input placeholder="Search account and videos" spellCheck={false} />
-                        <button className={cx('clear')}>
-                            <FontAwesomeIcon icon={faCircleXmark} />
-                        </button>
-                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                        <button className={cx('search-btn')}>
-                            <SearchIcon />
-                        </button>
-                    </div>
-                </HeadlessTippy>
+                
+                <Search/>
 
                 <div className={cx('action')}>
                     {currentUser ? (
---
+++ src/components/Layout/components/Search/index.js
@@ -0,0 +1,76 @@
+import { useState, useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import HeadlessTippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import AccountItem from '~/components/Layout/AccountItem';
+import { Wrapper as WrapperProper } from '~/components/Layout/Proper';
+import { SearchIcon } from '~/components/Icons';
+
+import styles from './Search.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Search() {
+    const [searchResult, setSearchResult] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+
+    const inputRef = useRef();
+    const [showResult, setShowResult] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1, 2, 3, 4]);
+        }, 1000);
+    }, []);
+    const handleClear = () => {
+        setSearchValue('');
+        setSearchResult([]);
+        inputRef.current.focus();
+    };
+    const handleHideResult = () => {
+        setShowResult(false);
+    };
+    return (
+        <HeadlessTippy
+            interactive
+            visible={showResult && searchResult.length > 0}
+            render={(attrs) => (
+                <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                    <WrapperProper>
+                        <h4 className={cx('search-title')}>Account</h4>
+                        <AccountItem />
+                        <AccountItem />
+                        <AccountItem />
+                        <AccountItem />
+                    </WrapperProper>
+                </div>
+            )}
+            onClickOutside={handleHideResult}
+        >
+            <div className={cx('search')}>
+                <input
+                    ref={inputRef}
+                    value={searchValue}
+                    placeholder="Search account and videos"
+                    spellCheck={false}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    onFocus={() => setShowResult(true)}
+                />
+                {!!searchValue && (
+                    <button className={cx('clear')} onClick={handleClear}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                )}
+
+                {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                <button className={cx('search-btn')}>
+                    <SearchIcon />
+                </button>
+            </div>
+        </HeadlessTippy>
+    );
+}
+
+export default Search;
